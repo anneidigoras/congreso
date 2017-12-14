818164df9e89788baf0b6d0638c245f3d8203a21
Benny96 <benatgalaizp@gmail.com>
2017-12-14 23:40:45 +0100

    Iniciando D2R

---
+++ app/cache/prod/twig/88/88e542ac17e15f4af53be072500fbe5776863dc06febbab22de665d2102ce0da.php
@@ -0,0 +1,219 @@
+<?php
+
+/* CongresoCongresoBundle:Congreso:list.html.twig */
+class __TwigTemplate_8c8565cc39fc091cf83ed86efa6eb02f20ab5e9617b9aef1df3de3fc45000034 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 2
+        $this->parent = $this->loadTemplate("CongresoCongresoBundle::layout.html.twig", "CongresoCongresoBundle:Congreso:list.html.twig", 2);
+        $this->blocks = array(
+            'javascripts' => array($this, 'block_javascripts'),
+            'title' => array($this, 'block_title'),
+            'body' => array($this, 'block_body'),
+            'sidebar' => array($this, 'block_sidebar'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "CongresoCongresoBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 5
+    public function block_javascripts($context, array $blocks = array())
+    {
+        // line 6
+        echo "            <script type=\"text/javascript\" src=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/jquery.js"), "html", null, true);
+        echo "\"></script>
+            <script type=\"text/javascript\" src=\"";
+        // line 7
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/jquery1.js"), "html", null, true);
+        echo "\"></script>
+            <script type=\"text/javascript\" src=\"";
+        // line 8
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/jquery.qtip-1.0.0-rc3.min.js"), "html", null, true);
+        echo "\"></script>
+            <script type=\"text/javascript\" src=\"";
+        // line 9
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/script1.js"), "html", null, true);
+        echo "\"></script>
+            <script type=\"text/javascript\" src=\"";
+        // line 10
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/script2.js"), "html", null, true);
+        echo "\"></script>
+";
+    }
+
+    // line 14
+    public function block_title($context, array $blocks = array())
+    {
+        // line 15
+        echo "Congreso Científico
+";
+    }
+
+    // line 18
+    public function block_body($context, array $blocks = array())
+    {
+        // line 19
+        echo "    <div id=\"lista-ocultar\">
+    ";
+        // line 20
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["tracks"] ?? null));
+        $context['_iterated'] = false;
+        foreach ($context['_seq'] as $context["_key"] => $context["track"]) {
+            // line 21
+            echo "    <holi class=\"track\">
+        <header>
+            <h3><a id=\"nombre-temas\" href=\"";
+            // line 23
+            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("congreso_track_arttrac", array("id" => $this->getAttribute($context["track"], "id", array()))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["track"], "campo", array()), "html", null, true);
+            echo "</a></h3>            
+        </header>
+        <div id=\"lista-ocultar1\">
+        ";
+            // line 26
+            $context["i"] = 0;
+            // line 27
+            echo "        ";
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(($context["articulos"] ?? null));
+            $context['_iterated'] = false;
+            foreach ($context['_seq'] as $context["_key"] => $context["articulo"]) {
+                // line 28
+                echo "            ";
+                if ((($this->getAttribute($context["track"], "id", array()) == $this->getAttribute($this->getAttribute($context["articulo"], "track", array()), "id", array())) && (($context["i"] ?? null) < 2))) {
+                    // line 29
+                    echo "                ";
+                    $context["i"] = (($context["i"] ?? null) + 1);
+                    // line 30
+                    echo "                <article class=\"articulo\">
+                <div class=\"separator\"></div>
+                    
+                    <p>";
+                    // line 33
+                    echo twig_escape_filter($this->env, $this->getAttribute($context["articulo"], "ParsearFecha", array()), "html", null, true);
+                    echo "</p>
+
+                    <header>
+                        <h2><a href=\"";
+                    // line 36
+                    echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("congreso_congreso_show", array("id" => $this->getAttribute($context["articulo"], "id", array()))), "html", null, true);
+                    echo "\">";
+                    echo twig_escape_filter($this->env, $this->getAttribute($context["articulo"], "titulo", array()), "html", null, true);
+                    echo "</a></h2>
+                    </header>
+
+                    <div class=\"snippet\">
+                        <p>";
+                    // line 40
+                    echo twig_escape_filter($this->env, (twig_slice($this->env, $this->getAttribute($context["articulo"], "cuerpo", array()), 0, 400) . "..."), "html", null, true);
+                    echo "</p>
+                        <p class=\"continue\"><a href=\"";
+                    // line 41
+                    echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("congreso_congreso_show", array("id" => $this->getAttribute($context["articulo"], "id", array()))), "html", null, true);
+                    echo "\">Seguir leyendo...</a></p>
+                    </div>
+
+                    <footer class=\"meta\">
+                        <p>Comentarios: ";
+                    // line 45
+                    echo twig_escape_filter($this->env, $this->getAttribute($context["articulo"], "numberofcomments", array()), "html", null, true);
+                    echo "</p>
+                    </footer>
+                </article>
+            ";
+                }
+                // line 49
+                echo "        ";
+                $context['_iterated'] = true;
+            }
+            if (!$context['_iterated']) {
+                // line 50
+                echo "            <p>No hay entradas en este blog</p>
+        ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['articulo'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 52
+            echo "
+        </div>
+    </holi>
+    ";
+            $context['_iterated'] = true;
+        }
+        if (!$context['_iterated']) {
+            // line 56
+            echo "        <p>aaaaa</p>
+
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['track'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 59
+        echo "    </div>
+";
+    }
+
+    // line 62
+    public function block_sidebar($context, array $blocks = array())
+    {
+        // line 63
+        echo "
+    <p>¿Quieres saber la fecha y hora actuales?</p>    
+
+    <button type=\"button\" id=\"boton-mostrar-ocultar\">Mostrar/Ocultar</button>
+
+    <div id=\"mostrar-ocultar\" style=\"display: none;\">
+        <p id=\"dia\">Jueves, 1 de enero de 1970</p>
+        <p id=\"reloj\">00:00:00</p>
+        <button type=\"button\" id=\"Actualizar\">Actualizar</button>
+    </div>
+
+
+    
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CongresoCongresoBundle:Congreso:list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  176 => 63,  173 => 62,  168 => 59,  160 => 56,  152 => 52,  145 => 50,  140 => 49,  133 => 45,  126 => 41,  122 => 40,  113 => 36,  107 => 33,  102 => 30,  99 => 29,  96 => 28,  90 => 27,  88 => 26,  80 => 23,  76 => 21,  71 => 20,  68 => 19,  65 => 18,  60 => 15,  57 => 14,  51 => 10,  47 => 9,  43 => 8,  39 => 7,  34 => 6,  31 => 5,  11 => 2,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "CongresoCongresoBundle:Congreso:list.html.twig", "C:\\xampp\\htdocs\\congreso\\src\\Congreso\\CongresoBundle\\Resources\\views\\Congreso\\list.html.twig");
+    }
+}
