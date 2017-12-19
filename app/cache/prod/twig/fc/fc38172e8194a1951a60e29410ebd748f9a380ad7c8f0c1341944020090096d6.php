1a0deeadd06992ecc8e938ef35f228ffadadba63
Benny96 <benatgalaizp@gmail.com>
2017-12-19 18:17:26 +0100

    Limpiando y validando CSS

---
+++ app/cache/prod/twig/fc/fc38172e8194a1951a60e29410ebd748f9a380ad7c8f0c1341944020090096d6.php
@@ -0,0 +1,130 @@
+<?php
+
+/* CongresoCongresoBundle:Congreso:contact.html.twig */
+class __TwigTemplate_92254ea6b49abb9d70037dace88da7248afba695572cd65a355218910910ac7e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 2
+        $this->parent = $this->loadTemplate("CongresoCongresoBundle::layout.html.twig", "CongresoCongresoBundle:Congreso:contact.html.twig", 2);
+        $this->blocks = array(
+            'javascripts' => array($this, 'block_javascripts'),
+            'title' => array($this, 'block_title'),
+            'body' => array($this, 'block_body'),
+            'tabla' => array($this, 'block_tabla'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "CongresoCongresoBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 4
+    public function block_javascripts($context, array $blocks = array())
+    {
+        // line 5
+        echo "            <script type=\"text/javascript\" src=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/jquery.js"), "html", null, true);
+        echo "\"></script>
+            <script type=\"text/javascript\" src=\"";
+        // line 6
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("js/script.js"), "html", null, true);
+        echo "\"></script>
+";
+    }
+
+    // line 10
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Contacto";
+    }
+
+    // line 12
+    public function block_body($context, array $blocks = array())
+    {
+        // line 13
+        echo "\t<div class=\"container\">
+\t    <header>
+\t       ";
+        // line 15
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\TranslationExtension')->getTranslator()->trans("<h1>Contacta con nosotros</h1>", array(), "messages");
+        // line 16
+        echo "\t    </header>
+\t    <p>A continuación puedes encontrar el repositorio para esta página de Congreso Científico subido a GitHub:</p>
+\t    <a href=\"https://github.com/anneidigoras/congreso\" target=\"_blank\">Congreso Científico (Repo)</a>
+\t    <div class=\"separator\"></div>
+\t    <p>Información detallada de los colaboradores del proyecto:</p>
+\t</div>
+ ";
+    }
+
+    // line 25
+    public function block_tabla($context, array $blocks = array())
+    {
+        // line 26
+        echo "
+\t<div class=\"container\">
+\t<table id=\"tabla\">
+
+
+\t\t\t
+\t\t<theader>
+\t\t\t<th name=\"Nombre\">Nombre</th><th name=\"Cargo\">Cargo</th><th name=\"Email\">Email</th><th name=\"LinkedIn\">LinkedIn</th>
+\t\t</theader>
+\t
+\t\t<tbody>
+\t\t\t<tr>
+\t\t\t\t<td class=\"num\" id=\"Nombre\" >Beñat Galdós</td><td class=\"num\" id=\"Cargo\">Desarrollador + Coordinador</td><td class=\"num\" id=\"Email\">benatgaldos@opendeusto.es</td><td class=\"num\" id=\"LinkedIn\" ><a href=\"https://www.linkedin.com/in/benatgaldos/\" target=\"_blank\">Referencia</a></td>
+\t\t\t</tr>
+\t\t\t<tr>
+\t\t\t\t<td class=\"num\" id=\"Nombre\" >Anne Idigoras</td><td class=\"num\" id=\"Cargo\">Desarrolladora</td><td class=\"num\" id=\"Email\">anneidigoras@opendeusto.es</td><td class=\"num\" id=\"LinkedIn\" ><a href=\"https://www.linkedin.com/in/anne-idigoras-pagola-772326a8/\" target=\"_blank\">Referencia</a></td>
+\t\t\t</tr>
+\t\t\t<tr>
+\t\t\t\t<td class=\"num\" id=\"Nombre\" >Imanol Echeverría</td><td class=\"num\" id=\"Cargo\">Desarrollador</td><td class=\"num\" id=\"Email\">imanol.echeverria@opendeusto.es</td><td class=\"num\" id=\"LinkedIn\" ><a href=\"https://www.linkedin.com/in/imanol-echeverr%C3%ADa-franco-67617513b/\" target=\"_blank\">Referencia</a></td>
+\t\t\t</tr>
+\t\t\t<tr>
+\t\t\t\t<td class=\"num\" id=\"Nombre\" >Garikoitz Bereciartua</td><td class=\"num\" id=\"Cargo\">Desarrollador</td><td class=\"num\" id=\"Email\">gari.bereciartua@opendeusto.es</td><td class=\"num\" id=\"LinkedIn\" ><a href=\"https://www.linkedin.com/in/garikoitz-bereciartua-ram%C3%B3n-a40322152/\" target=\"_blank\">Referencia</a></td>
+\t\t\t</tr>
+\t\t</tbody>
+\t</table>
+\t</div>
+
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CongresoCongresoBundle:Congreso:contact.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  73 => 26,  70 => 25,  60 => 16,  58 => 15,  54 => 13,  51 => 12,  45 => 10,  39 => 6,  34 => 5,  31 => 4,  11 => 2,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "CongresoCongresoBundle:Congreso:contact.html.twig", "C:\\xampp\\htdocs\\congreso\\src\\Congreso\\CongresoBundle\\Resources\\views\\Congreso\\contact.html.twig");
+    }
+}
